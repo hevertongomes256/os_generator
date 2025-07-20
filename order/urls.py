6990d1e26462d6c915ede3335567c30fb39b07ba
hevertongomes <heverton.gomes@franq.com.br>
2025-07-20 12:01:24 -0300

    feat:created form update

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,9 @@
 # orders/urls.py
 from django.urls import path
-from .views import OrderListView, order_create
+from .views import OrderListView, order_edit, order_create
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='orders-list'),
     path('orders/new/', order_create, name='order-create'),
+    path('orders/<int:pk>/edit/', order_edit, name='order-edit'),
 ]
