1da3e7fc481a99f4da2579f97b9b6d47de29fedd
hevertongomes <heverton.gomes@franq.com.br>
2025-08-03 11:07:11 -0300

    feat:update admin cases

--- person/admin.py
+++ person/admin.py
@@ -1,27 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import PersonCreateForm, PersonChangeForm
 from .models import Person, Address
+from .forms import PersonCreationForm
 
 # Register your models here.
 
 
-class PersonAdmin(UserAdmin):
-    add_form = PersonCreateForm
-    form = PersonChangeForm
-    model = Person
-    list_display = ('username', 'email', 'first_name', 'last_name', 'person_type', 'has_usable_password_display')
-    fieldsets = (
-        (None, {'fields': ('username', 'email', 'first_name', 'last_name', 'person_type',)}),
-    )
-    add_fieldsets = (
-        (None, {'fields': ('username', 'email', 'first_name', 'last_name', 'person_type', 'owner',)}),
-    )
-
-    def has_usable_password_display(self, obj):
-        return obj.has_usable_password()
-    has_usable_password_display.boolean = True
-    has_usable_password_display.short_description = 'Senha configurada?'
+class PersonAdmin(admin.ModelAdmin):
+    form = PersonCreationForm
 
 
 admin.site.register(Person, PersonAdmin)
--- person/forms.py
+++ person/forms.py
@@ -1,26 +1,28 @@
 from django import forms
 from .models import Person
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
-class PersonCreateForm(UserCreationForm):
+class PersonCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirme a senha', widget=forms.PasswordInput)
+
     class Meta:
         model = Person
-        fields = ['username', 'email', 'first_name', 'last_name', 'person_type']
+        fields = ('username', 'first_name', 'last_name', 'email', 'person_type', 'phone')
 
-    def clean(self):
-        cleaned_data = super().clean()
-        person_type = cleaned_data.get('person_type')
-        password = cleaned_data.get('password')
-        if person_type == 'logista' and not password:
-            raise forms.ValidationError("Logista precisa de senha.")
-        return cleaned_data
+    def clean_password2(self):
+        p1 = self.cleaned_data.get('password1')
+        p2 = self.cleaned_data.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError("As senhas não conferem")
+        return p2
 
-
-class PersonChangeForm(UserChangeForm):
-    class Meta:
-        model = Person
-        fields = ['username', 'email', 'first_name', 'last_name', 'person_type']
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
 class PersonForm(forms.ModelForm):
@@ -28,9 +30,38 @@ class PersonForm(forms.ModelForm):
         model = Person
         fields = ['first_name', 'last_name', 'email', 'phone']
 
+    def clean(self):
+        cleaned_data = super().clean()
+        phone = cleaned_data.get('phone', '')
+        if phone and len(phone) < 10:
+            self.add_error('phone', 'Informe um telefone válido.')
+        return cleaned_data
+
+    def generate_username(self, first_name, last_name, phone):
+        username_base = f"{first_name.strip()}{last_name.strip()}".lower()
+        digits = ''.join(filter(str.isdigit, phone))
+        suffix = digits[-4:] if len(digits) >= 4 else digits
+        username = username_base + suffix
+
+        unique_username = username
+        counter = 1
+        while Person.objects.filter(username=unique_username).exists():
+            unique_username = f"{username}{counter}"
+            counter += 1
+
+        return unique_username
+
     def save(self, commit=True):
         instance = super().save(commit=False)
         instance.person_type = 2
+
+        first_name = self.cleaned_data.get('first_name', '')
+        last_name = self.cleaned_data.get('last_name', '')
+        phone = self.cleaned_data.get('phone', '')
+
+        username = self.generate_username(first_name, last_name, phone)
+        instance.username = username
+
         if commit:
             instance.save()
         return instance
